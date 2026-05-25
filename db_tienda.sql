980d7aa527ffc079a1a05426f2165ae98fd77fc9
Thomas-Arnaiz <tarnaiz@alumnos.exa.unicen.edu.ar>
2026-05-24 23:19:47 -0300

    Update db_tienda.sql

--- db_tienda.sql
+++ db_tienda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-04-2026 a las 01:12:04
+-- Tiempo de generación: 25-05-2026 a las 04:19:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_tienda`
+-- Base de datos: `tienda`
 --
 
 -- --------------------------------------------------------
@@ -39,7 +39,12 @@ CREATE TABLE `ropa` (
 --
 
 INSERT INTO `ropa` (`ropa_id`, `nombre`, `precio`, `talle_id`) VALUES
-(1, 'Camiseta Titular River Plate 25/26 Betano', 150, 2);
+(1, 'Camiseta Titular River Plate', 150009, 1),
+(5, 'remera', 333, 1),
+(6, 'Camiseta Titular River Plate', 2000, 1),
+(7, 'remera', 333, 1),
+(9, 'remera', 426224, 2),
+(11, 'remera', 41255252, 2);
 
 -- --------------------------------------------------------
 
@@ -49,17 +54,41 @@ INSERT INTO `ropa` (`ropa_id`, `nombre`, `precio`, `talle_id`) VALUES
 
 CREATE TABLE `talles` (
   `talle_id` int(11) NOT NULL,
-  `nombre_talle` varchar(3) NOT NULL
+  `nombre_talle` varchar(3) NOT NULL,
+  `imagen_url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `talles`
 --
 
-INSERT INTO `talles` (`talle_id`, `nombre_talle`) VALUES
-(1, 's'),
-(2, 'm'),
-(3, 'l');
+INSERT INTO `talles` (`talle_id`, `nombre_talle`, `imagen_url`) VALUES
+(1, 'l', ''),
+(2, 'm', ''),
+(3, 'xs', ''),
+(4, 'xl', ''),
+(7, 'xl', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(350) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
+(1, 'ivan', '$2y$10$.Z.R5twM5Oi13w/8GmxAJecrg1BR0I4bUvYm/RNFVHD'),
+(2, 'ivan2', '$2y$10$T8pkbqwrmauvuVWG.O3YCOUZZESg88ho.EOmfB3gj/JEjc1vfiwIW'),
+(3, 'webadmin', '$2y$10$ih/G8z9IRTBdAJ9hCLPmjOYnG/ECUGAUP1CVtgZym4kgCICnI9wiu');
 
 --
 -- Índices para tablas volcadas
@@ -70,13 +99,19 @@ INSERT INTO `talles` (`talle_id`, `nombre_talle`) VALUES
 --
 ALTER TABLE `ropa`
   ADD PRIMARY KEY (`ropa_id`),
-  ADD UNIQUE KEY `talle_id` (`talle_id`);
+  ADD KEY `ropa_ibfk_1` (`talle_id`);
 
 --
 -- Indices de la tabla `talles`
 --
 ALTER TABLE `talles`
   ADD PRIMARY KEY (`talle_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -86,13 +121,19 @@ ALTER TABLE `talles`
 -- AUTO_INCREMENT de la tabla `ropa`
 --
 ALTER TABLE `ropa`
-  MODIFY `ropa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ropa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `talles`
 --
 ALTER TABLE `talles`
-  MODIFY `talle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `talle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
